eeaa6db0ed8a67e922ba5d6edd6a87103903b5f8
Michael Elleby <michael@softwareahead.dk>
2022-10-18 15:19:05 +0200

    Implemented program demonstrating the problem

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace DurableTaskBugDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebJobs(config =>
+                {
+                    config.AddDurableTask(options => { options.HubName = "MyTaskHub"; });
+                })
+                .UseConsoleLifetime();
+
+            var host = hostBuilder.Build();
+
+            using (host)
+            {
+                host.Run();
+            }
+        }
+    }
+}
